7e1e7e16d657ebabedeeff94700aeed938d8c26c
dd2558 <dd2558@naver.com>
2024-03-13 17:19:02 +0900

    DAO,DTO 추가

---
+++ src/main/java/file/FileDTO.java
@@ -0,0 +1,19 @@
+package file;
+
+public class FileDTO {
+	
+	String fileName;
+	String fileRealName;
+	public String getFielName() {
+		return fileName;
+	}
+	public void setFielName(String fielName) {
+		this.fileName = fielName;
+	}
+	public String getFileRealName() {
+		return fileRealName;
+	}
+	public void setFileRealName(String fileRealName) {
+		this.fileRealName = fileRealName;
+	}
+}
